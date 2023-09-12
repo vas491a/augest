74817279d65a9a110842ddb1eecb4c3fbcb8a052
anaparthi srinivas <srinivasanaparthi491@gmail.com>
2023-09-12 13:18:44 +0530

    secndcommittf

---
+++ c4-ami-datasource.tf
@@ -0,0 +1,22 @@
+# grt latest ami id for amzon linux2 os
+data "aws_ami" "amzlinux2" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-gp2"]
+  }
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+}
